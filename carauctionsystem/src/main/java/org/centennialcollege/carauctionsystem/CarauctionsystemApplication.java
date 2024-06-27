94d1b0cf39233794a357cd13bb3c799b1ab421a3
chun <kchan307@my.centennialcollege.ca>
2024-06-27 15:33:14 -0400

    setup Springboot backend

---
+++ carauctionsystem/src/main/java/org/centennialcollege/carauctionsystem/CarauctionsystemApplication.java
@@ -0,0 +1,13 @@
+package org.centennialcollege.carauctionsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarauctionsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarauctionsystemApplication.class, args);
+	}
+
+}
